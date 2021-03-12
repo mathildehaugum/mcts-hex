a0d02044d79be8e0961695c05c496e39fbbcae66
mathildehaugum <mathilde_haugum@hotmail.com>
2021-03-12 14:34:03 +0100

    Add cell state representation

---
+++ environment/cell.py
@@ -0,0 +1,39 @@
+class Cell:
+    """ Class for making a peg solitaire piece which is a Cell object"""
+    def __init__(self, row, column):
+        self.location = (row, column)
+        self.name = "Cell" + str(row) + str(column)
+        self.neighbor_list = []
+        self.cell_state = (0, 0)  # tells the state of the cell, where (0, 0) is empty, (1, 0) belong to player 1 and (0, 1) belong to player 2
+
+    def __str__(self):
+        """ Changes Cell object representation to string format to make debugging easier"""
+        return self.name
+
+    def __repr__(self):
+        """ Changes Cell object representation to string format to make debugging easier"""
+        return self.name
+
+    def add_neighbor(self, neighbor_cell):
+        """ Adds neighbor relationship to both connected cells"""
+        self.neighbor_list.append(neighbor_cell)
+        neighbor_cell.neighbor_list.append(self)
+
+    def get_neighbors(self):
+        """ Gets list of neighbor cells to current cell"""
+        return self.neighbor_list
+
+    def get_cell_state(self):
+        """ Returns state of cell, where (0, 0) = empty, (1, 0) = belong to player 1 and (0, 1) = belong to player 2"""
+        return self.cell_state
+
+    def set_cell_state(self, new_state):
+        """ Change state of cell, where (0, 0) is empty, (1, 0) belong to player 1 and (0, 1) belong to player 2"""
+        if len(new_state) == 2:
+            self.cell_state = new_state
+
+    def get_location(self):
+        """ Returns location of cell as (row, col)"""
+        return self.location
+
+
