bfc2f7ff706537c75ca75bf885b359362459a63c
mathildehaugum <mathilde_haugum@hotmail.com>
2021-03-18 10:33:27 +0100

    Add nim game logic

--- environment/nim_game.py
+++ environment/nim_game.py
@@ -1,32 +1,41 @@
-from environment.game_board import *
+from environment.game_board import HexagonalDiamondGrid
+from visualization import *
+
 
 class Nim:
     """ Class for performing a version of Nim where two players takes turn in adding a number of pieces
      between 1 and K. The game is over when only one empty cell remains and the current player is the loser.
-     This version matches better with Hex (compared to ordinary Nim where pieces are removed)"""
-    def __init__(self, board, nim_n, nim_k=1, ):
-        self.num_pieces = nim_n
+     This version matches better with Hex compared to ordinary Nim where pieces are removed"""
+    def __init__(self, board_size, nim_k=1):
+        self.size = board_size
+        self.num_pieces = self.size * self.size
         self.max_piece_adding = nim_k
-        self.board = board
 
-    def perform_actions(self, cell_list, player):
-        for i in range(len(cell_list)):
-            action = [cell_list[i], player]
-            print(action)
+        self.board = HexagonalDiamondGrid(self.size)
+
+    def get_board(self):
+        return self.board
+
+    def perform_action(self, cell_locations, player):
+        for i in range(len(cell_locations)):
+            action = [cell_locations[i], player]
             self.board.perform_action(action)
 
     def is_winning_state(self):
         return self.board.get_pegs_nums[0] == 1
 
+    def get_legal_actions(self):
+        self.board.get_legac_actions()
+
 
 if __name__ == '__main__':
-    board = HexagonalDiamondGrid(4)
-    nim_game = Nim(board, 16, 3)
-    print(board.get_binary_state())
-    nim_game.perform_actions([(0, 0), (1, 0), (1, 1)], 1)
-    print(board.get_binary_state())
-    nim_game.perform_actions([(2, 1), (2, 2), (3, 3)], 2)
-    print(board.get_binary_state())
+    nim_game = Nim(4, 3)
+    visualizer = Visualizer(nim_game.get_board(), 1000, nim_game)
+
+    actions = [([(0, 0), (1, 0), (1, 1)], 1), ([(2, 1), (2, 2), (3, 3)], 2)]
+    visualizer.visualize_last_game(actions)
+
+
 
 
 
