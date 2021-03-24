369c5844204325f4ea5644da37f569029559ad6e
mathildehaugum <mathilde_haugum@hotmail.com>
2021-03-24 15:36:01 +0100

    Add node to represent state

--- agent/node.py
+++ agent/node.py
@@ -3,22 +3,20 @@ class Node:
         """Initialize a node with its state and the given player that performs the given
          action from the parent node to produce this node"""
         self.name = "State: [" + str(state) + "], Action: " + str(action)
-        self.state = state
-        self.action = action
-        self.player = action[1]
+        self.state = state  # State when this node is root
+        self.action = action  # Action taken from root leading to this child node
         self.parent = None
         self.child_nodes = []
-
-        # Values updated during MCTS backpropagation:
         self.node_value = 0  # the value of the action that leads to this node
         self.node_counter = 0  # counts the number of times the node is visited during the search
+        self.player = action[1]  # The player that perform the action that produces this node
 
     def __str__(self):
         """ Changes Node object representation to string format to make debugging easier"""
         return self.name
 
     def __repr__(self):
-        """ Changes Cell object representation to string format to make debugging easier"""
+        """ Changes Node object representation to string format to make debugging easier"""
         return self.name
 
     def set_parent(self, parent_node):
@@ -40,6 +38,12 @@ class Node:
 
     def get_player(self):
         return self.player
+
+    def set_player(self, player):
+        self.player = player
+
+    def get_action(self):
+        return self.action
 
     def update_counter(self):
         self.node_counter += 1
