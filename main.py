d51884ded061f412a7b8b8cee47833d8dfa48386
mathildehaugum <mathilde_haugum@hotmail.com>
2021-03-24 15:39:21 +0100

    connect all components

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+import config
+from environment.state_manager import StateManager
+from agent.actor import Actor
+from game_agent import GameAgent
+from visualization import Visualizer
+from agent.replay_buffer import ReplayBuffer
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    state_manager = StateManager(config.game_type, config.board_size, nim_k=1)
+    actor = Actor(config.learning_rate, config.epsilon, config.decay_rate, config.board_size, config.nn_dims, config.activation, config.optimizer, config.loss_function)
+    visualizer = Visualizer(state_manager.game.get_board(), state_manager.game, config.visualization_speed, config.visualization_interval)
+    replay_buffer = ReplayBuffer()
+    game_agent = GameAgent(actor, config.save_interval, state_manager, visualizer, replay_buffer, config.starting_player, config.num_episodes, config.num_simulations)
+    game_agent.run()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+
