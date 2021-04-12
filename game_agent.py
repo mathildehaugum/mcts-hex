82f1f9680847514d3fb8c81ec559a82066d30162
mathildehaugum <mathilde_haugum@hotmail.com>
2021-04-12 18:59:00 +0200

    Refactor

--- game_agent.py
+++ game_agent.py
@@ -3,18 +3,20 @@ from agent.mcts import MonteCarloTreeSearch
 from agent.replay_buffer import ReplayBuffer
 from environment.state_manager import StateManager
 from agent.node import Node
+import random
 
 
 class GameAgent:
-    def __init__(self, actor, save_interval, state_manager, visualizer, replay_buffer, starting_player, num_episodes, num_simulations):
+    def __init__(self, actor, save_interval, state_manager, visualizer, replay_buffer, starting_player, num_episodes, num_simulations, dir_num=0):
         self.actor = actor  # 3: ANET with randomly initialized parameters
-        self.save_interval = save_interval
+        self.save_interval = num_episodes/(save_interval-1)  # -1 since the first episode (i.e. num 0) is stored
         self.state_manager = state_manager
         self.visualizer = visualizer
         self.replay_buffer = replay_buffer
         self.starting_player = starting_player
         self.num_episodes = num_episodes
         self.num_simulations = num_simulations
+        self.dir_num = dir_num
 
     def run(self):
 
@@ -24,7 +26,7 @@ class GameAgent:
         # 4: For each episode (i.e. number_actual_games)
         for current_episode in range(1, self.num_episodes + 1):
 
-            actions = [] # Used in visualization
+            actions = []  # Used in visualization
 
             # 4a: Initialize game board to empty board
             self.state_manager.init_game()
@@ -32,16 +34,18 @@ class GameAgent:
             # 4b: Initialize starting_state and set root
             state = self.state_manager.get_state()
 
-            # 4c: Initialize MCT to root
-            player = self.starting_player
-            # CHANGE: MAKE NEW ROOT FROM STATE AND PLAYER AND GIVE THIS AS INPUT TO MCTS?
+            # 4c: Initialize MCTS to root
+            if self.starting_player == 0:
+                player = random.choice([1, 2])
+            else:
+                player = self.starting_player
             mcts = MonteCarloTreeSearch(self.actor, self.state_manager, state, player)
 
             # 4D: While episode is not in final state (i.e. no player hos won)
             while not self.state_manager.is_winning_state(state):
                 for simulation in range(1, self.num_simulations + 1):
                     leaf_node = mcts.tree_search()
-                    mcts.leaf_node_expansion(leaf_node)  # OBSOBS: FJERNE RETURNERING? JA, GJORT!
+                    mcts.leaf_node_expansion(leaf_node)
                     final_evaluation = mcts.leaf_evaluation(leaf_node)
                     mcts.backpropagation(leaf_node, final_evaluation)
 
@@ -50,12 +54,11 @@ class GameAgent:
                 self.replay_buffer.add_case(mcts.get_root(), D)
 
                 self.state_manager.reset_state(state)  # Avoid that any cell states are changed during leaf_node evaluation (i.e. that search actions actually are performed
-                chosen_child = mcts.select_action(player, D)
+                chosen_child = self.state_manager.select_action(mcts.get_root(), player, D)
                 actions.append(chosen_child.get_action())  # used in visualization
 
                 self.state_manager.perform_action(chosen_child.get_action())
                 state = self.state_manager.get_state()
-                print(state)
                 player = 1 if player == 2 else 2
                 mcts.set_root(chosen_child)
 
@@ -63,27 +66,18 @@ class GameAgent:
             self.actor.train(self.replay_buffer.get_random_minibatch())
             self.actor.decay_epsilon()
 
+            if current_episode == self.num_episodes:
+                self.actor.set_epsilon(0)
+
             # 4f: Save ANET if condition is fulfilled
             if current_episode == 1 or current_episode % self.save_interval == 0:
-                self.actor.save(current_episode)
+                self.actor.save(current_episode, self.dir_num)
 
             # Visualize last episode
-            if current_episode % self.visualizer.get_interval():
-                print(actions)
-                self.visualizer.visualize(actions)
-        self.actor.plot_loss_and_accuracy()
+            if current_episode == 1 or current_episode % self.visualizer.get_interval() == 0:
+                self.visualizer.visualize(actions, current_episode)
+            print("Episode : " + str(current_episode) + ", epsilon: " + str(self.actor.epsilon))
 
-
-"""if __name__ == '__main__':
-    actor = Actor()
-    sm = StateManager("HEX", 3)
-    buffer = ReplayBuffer()
-    starting_player = 1
-    num_episodes = 3
-    num_simulations = 2
-
-    agent = GameAgent(actor, 0, sm, None, buffer, starting_player, num_episodes, num_simulations)
-    agent.run()"""
 
 
 
