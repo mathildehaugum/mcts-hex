dd6f15d2f6f8305e53022b95e447590a95baab88
mathildehaugum <mathilde_haugum@hotmail.com>
2021-04-12 18:58:33 +0200

    Rotate game board to create diamond structure

--- visualization.py
+++ visualization.py
@@ -4,7 +4,6 @@ from celluloid import Camera
 from environment.hex_game import Hex
 
 
-# OBS OBS OBS : visualized board should be diamond, not rectangle! NEED TO FIX THIS!
 class Visualizer:
     def __init__(self, board_size,  visualization_speed, visualization_interval):
         self.game = Hex(board_size)
@@ -19,10 +18,16 @@ class Visualizer:
 
     def init_board_visualizer(self):
         G = nx.Graph()
-        for current_cell in self.board.get_cells():
-            x_pos = current_cell.get_location()[0]  # Positive x-pos to not horizontally flip graph
-            y_pos = -current_cell.get_location()[1]  # Negative y-pos to vertically flip graph
+        row = 0
+        col = self.game.size - 1  # Since location placements start with 0, the size have to be adjusted
+        for current_cell in self.board.get_cells():  # Cells are given in order of increasing col, and then increasing row [00, 01, 02, 10, 11, 12, 20, 21, 22]
+            if row == self.game.size:
+                row = 0
+                col -= 1
+            y_pos = -(current_cell.get_location()[0] + row)  # Positive x-pos to not horizontally flip graph
+            x_pos = (current_cell.get_location()[1] + col)  # Negative y-pos to vertically flip graph
             G.add_node(current_cell, pos=(x_pos, y_pos))
+            row += 1
             for neighbor_cell in current_cell.get_neighbors():
                 G.add_edge(current_cell, neighbor_cell)  # edges can be added before nodes (networkx docs)
         return G, nx.get_node_attributes(G, 'pos')
@@ -38,21 +43,25 @@ class Visualizer:
                                node_color='red', linewidths=2, edgecolors='black')
         nx.draw_networkx_nodes(self.G, self.pos, nodelist=black_pegs,
                                node_color='black', linewidths=2, edgecolors='black')
+        plt.ylabel("P1                                                P2")
+        plt.title('Hex')
+
         self.camera.snap()
 
-    def animate_visualiser(self):
+    def animate_visualiser(self, current_episode):
         animation = self.camera.animate(interval=self.speed, repeat=False)
-        animation.save('images/animation.gif')
+        animation.save('images/animation_' + str(current_episode) + '.gif')
         plt.show(block=False)
         plt.close()
 
-    def visualize(self, game_actions):
+    def visualize(self, game_actions, current_episode):
         """ Visualizes the performed actions and resulting states in the given episode"""
+        self.board.reset_board()
         self.draw_board()
         for action in game_actions:
             self.game.perform_action(action)
             self.draw_board()
-        self.animate_visualiser()
+        self.animate_visualiser(current_episode)
 
 
 
