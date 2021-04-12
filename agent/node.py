c9c55470d87c442cf7813f71847ee7c5bc4f0e6d
mathildehaugum <mathilde_haugum@hotmail.com>
2021-04-12 18:56:29 +0200

    Refactor

--- agent/node.py
+++ agent/node.py
@@ -1,7 +1,7 @@
 class Node:
     def __init__(self, state, action):
-        """Initialize a node with its state and the given player that performs the given
-         action from the parent node to produce this node"""
+        """ Class for making nodes that represents different states in the game. A node  will contain
+        a state, the action that produced the state and the current player that will chose an action from this state. """
         self.name = "State: [" + str(state) + "], Action: " + str(action)
         self.state = state  # State when this node is root
         self.action = action  # Action taken from root leading to this child node
@@ -9,7 +9,7 @@ class Node:
         self.child_nodes = []
         self.node_value = 0  # the value of the action that leads to this node
         self.node_counter = 0  # counts the number of times the node is visited during the search
-        self.player = action[1]  # The player that perform the action that produces this node
+        self.player = action[1]  # The player that performs an action from the state of this node (i.e. decides next state)
 
     def __str__(self):
         """ Changes Node object representation to string format to make debugging easier"""
@@ -20,44 +20,57 @@ class Node:
         return self.name
 
     def set_parent(self, parent_node):
+        """ Sets the parent node containing the parent state"""
         self.parent = parent_node
 
     def get_parent(self):
+        """ Returns the parent node containing the parent state"""
         return self.parent
 
     def add_child(self, child_node):
+        """ Create parent-child relationships used during MCTS leaf expansion"""
         self.child_nodes.append(child_node)
         child_node.set_parent(self)
 
     def get_child_nodes(self):
+        """ Returns the child nodes containing the available child states from the state of the current node"""
         return self.child_nodes
 
     def get_state(self):
-        """Returns the state of the board game when this node is the root node"""
+        """ Returns the state of the board game when this node is the root node"""
         return self.state
 
     def get_player(self):
+        """ Returns the player that will chose an action from the state of this node"""
         return self.player
 
     def set_player(self, player):
+        """ Sets the player that will chose an action from the state of this node"""
         self.player = player
 
     def get_action(self):
+        """ Return the action that produced this node"""
         return self.action
 
     def update_counter(self):
+        """ Increase the counter of the node, representing how many times the state has been
+        visited during MCTS. The counters are updated during backpropagation"""
         self.node_counter += 1
 
     def get_counter(self):
+        """ Returns the counter of the node, used to produce the action probability distribution in MCTS"""
         return self.node_counter
 
     def get_parent_counter(self):
+        """ Returns the counter of the parent node, used to calculate the value of the node during MCTS"""
         return self.parent.get_counter()
 
     def get_value(self):
+        """ Returns the value of the node, used to decide the path during the tree search step of MCTS"""
         return self.node_value
 
     def update_value(self, value):
+        """ Updates the value of the node"""
         self.node_value = value
 
 
