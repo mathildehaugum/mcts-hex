77f2d7295f1fc468c235cfbce7cfa831b49e00d0
mathildehaugum <mathilde_haugum@hotmail.com>
2021-04-13 12:04:08 +0200

    Allow tournament for demo and topp

--- tournament.py
+++ tournament.py
@@ -1,30 +1,34 @@
 import glob
 import os
 import random
-import config
+from config import topp_config, demo_config
 from agent.actor import Actor
 from environment.state_manager import StateManager
 from visualization import Visualizer
 
 
 class Topp:
-    def __init__(self):
+    def __init__(self, config_type):
+        if config_type == "topp":
+            config = topp_config
+        else:
+            config = demo_config
         self.state_manager = StateManager(config.game_type, config.board_size, nim_k=1)
         self.visualizer = Visualizer(config.board_size, config.visualization_speed, config.visualization_interval)
         self.G = config.num_games
         self.load_directory = config.load_path
-        self.agents = self.init_agents()
+        self.agents = self.init_agents(config)
         self.results = self.init_results()
 
-    def init_agents(self):
+    def init_agents(self, config):
         agents = []
         os.chdir(self.load_directory)  # Changes current working directory to load_directory
         file_list = glob.glob("*.h5")  # Returns all files in directory with ending .h5
         file_list = sorted(file_list, key=return_episode_num)
         for file in file_list:
-            filepath = "../models/" + file  # The path is used in actor which is placed in agent
-            actor = Actor(config.learning_rate, config.epsilon, config.decay_rate, config.board_size, config.nn_dims, config.activation, config.optimizer, config.loss_function, filepath)
-            actor.load(filepath)
+            filename = "../models/" + file  # The path is used in actor which is placed in agent
+            actor = Actor(config.learning_rate, config.epsilon, config.decay_rate, config.board_size, config.nn_dims, config.activation, config.optimizer, config.loss_function, filename)
+            actor.load(filename)
             agents.append(actor)
         return agents
 
@@ -43,7 +47,7 @@ class Topp:
                 p1_total_win = 0
                 p2_total_win = 0
                 for game_num in range(self.G):
-                    p1_wins, p2_wins = self.play_game(p1, p2)
+                    p1_wins, p2_wins, actions = self.play_game(p1, p2)
                     p1_total_win += p1_wins
                     p2_total_win += p2_wins
                 print(p1.name + ": " + str(p1_total_win) + " wins, " + p2.name + ": " + str(p2_total_win) + " wins")
@@ -72,7 +76,7 @@ class Topp:
         winning_agent = players[change_player(player)-1]  # Since player is changed in end of while, the winning player at winning state is the previous player
         #print(p1.name + " vs. " + p2.name + ", winner: " + winning_agent.name + ", actions: " + str(actions))
         self.results[winning_agent.name] += 1
-        return p1_wins, p2_wins
+        return p1_wins, p2_wins, actions
 
     def print_result(self):
         print("Final results: ")
