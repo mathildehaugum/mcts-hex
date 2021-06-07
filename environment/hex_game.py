d0749498e7de06df0f45a39712ec824082731e85
mathildehaugum <mathilde_haugum@hotmail.com>
2021-06-07 10:48:19 +0200

    Update documentation

--- environment/hex_game.py
+++ environment/hex_game.py
@@ -9,9 +9,11 @@ class Hex:
         self.board = HexagonalDiamondGrid(self.size)
 
     def get_board(self):
+        """ Returns the game board"""
         return self.board
 
     def get_current_state(self):
+        """ Returns the state of the board containing the cell states of the board (e.g. [0, 0 , 1, 0, 2, 0, 1, 2, 0])"""
         return self.board.get_binary_state()
 
     def get_next_state(self, state, action):
@@ -21,24 +23,20 @@ class Hex:
         return self.board.get_binary_state()
 
     def get_legal_cells(self, state):
-        """Given a state that lists the states of the cell on the board (e.g. [(0, 0), (1, 0), (0, 0), ...],
-        this method will return the legal actions for this game state by updating the board cells and
-        then returning the cells that are not owned by any player"""
+        """ Returns the legal actions for the given game state by updating the board cell states and
+        then returning the cells that are not owned by any player (i.e. cell_state == 0)"""
         self.set_cell_states(state)
         return self.board.get_empty_cells()
 
     def set_cell_states(self, cell_states):
-        """Cell_states is a list of the board cells state values, e.g. [(0, 1), (0, 0), (1, 0), (0, 0), ...]
-         This method will receive such a list and changes the states of the currents board cells to
-         the states given by the list"""
+        """ Changes the states of the currents board cells to the given state"""
         board_cells = self.board.get_cells()
-        # TODO: LEGG TIL EXCEPTION MANAGEMENT (sjekk at cell_states har like mange celler som board_cells)
         for i in range(len(board_cells)):
             board_cells[i].set_cell_state(cell_states[i])
 
     def perform_action(self, action):
         """Perform action by changing the state of the cell at the given location to a new state
-        depending on the player: (1, 0): player 1, (0, 1): player 2"""
+        depending on the player: player 1 = 1 or player 2 = 2"""
         cell_location, player = action[0], action[1]
         self.board.perform_action(cell_location, player)
 
@@ -47,7 +45,7 @@ class Hex:
         sides to the other. This is checked by iterating over the starting locations for each player sides and
         if one of these locations belong to the player that owns this side, a depth first search is performed to see
         if this piece is the beginning of a path to the other side.
-        player 1 has bottom-left and top-right, while player 2 has bottom-right and top-left"""
+        Player 1 has bottom-left and top-right side, while player 2 has bottom-right and top-left side"""
         for starting_location in range(self.size):
             p1_start_cell, p2_start_cell = self.board.get_cell([0, starting_location]), self.board.get_cell([starting_location, 0])
             if p1_start_cell.get_cell_state() == 1:
@@ -64,7 +62,7 @@ class Hex:
         cell_stack = collections.deque()  # implement python stack
         cell_stack.append(starting_cell)
         visited = set()  # set to keep track of visited cells, faster to determine if object is present
-        while len(cell_stack) != 0:
+        while len(cell_stack) > 0:
             current_cell = cell_stack.pop()
             cell_state = current_cell.get_cell_state()
             cell_row, cell_col = current_cell.get_location()
