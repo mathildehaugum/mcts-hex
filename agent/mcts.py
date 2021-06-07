36f95adc693facdf942174da3f9ff8d0c1d1f3e1
mathildehaugum <mathilde_haugum@hotmail.com>
2021-06-07 10:42:15 +0200

    Implement tree search

--- agent/mcts.py
+++ agent/mcts.py
@@ -1,7 +1,7 @@
 from numpy import log, sqrt
 import numpy as np
 from agent.node import Node
-from math import floor, sqrt
+from math import sqrt
 
 
 class MonteCarloTreeSearch:
@@ -31,35 +31,35 @@ class MonteCarloTreeSearch:
 
     def tree_search(self):
         """ Traverse the tree from a root to a leaf node by using the tree policy. As long as the root does not produce
-        a winning state (i.e. is a final node) and it has childrens (i.e. is not a leaf node), the method will update the
+        a winning state (i.e. is a final node) and it has children (i.e. is not a leaf node), the method will update the
         values of the child nodes and then use the Tree policy (min-max) to choose the next node. It returns the chosen leaf node"""
-        current_root = self.root
-        child_nodes = current_root.get_child_nodes()
-        while not self.state_manager.is_winning_state(current_root.get_state()) and len(child_nodes) > 0:
-            action_values = {}
-            for child in child_nodes:  # Update value of each child node before using tree policy to choose the next root
-                if current_root.get_player() == 1:
+        current_node = self.root
+        child_nodes = current_node.get_child_nodes()
+        while not self.state_manager.is_game_over(current_node.get_state()) and len(child_nodes) > 0:
+            child_values = {}
+            for child in child_nodes:  # Update value of each child node before using tree policy to choose the next root. Values are stored in child_values to avoid updating the child values outside backpropagation
+                if current_node.get_player() == 1:
                     u = self.c * sqrt(log(child.get_parent_counter())/(1 + child.get_counter()))
-                    action_values[child] = child.get_value() + u
-                elif current_root.get_player() == 2:
+                    child_values[child] = child.get_value() + u
+                elif current_node.get_player() == 2:
                     u = self.c * sqrt(log(child.get_parent_counter())/(1 + child.get_counter()))
-                    action_values[child] = child.get_value() - u
+                    child_values[child] = child.get_value() - u
             # Tree policy: choose action (and hence next root) that maximize value for P1 or minimize value for P2
-            if current_root.get_player() == 1:  # TODO: IF ACTION_VALUES CONTAINS SIMILAR VALUES, THE FIRST IS ALWAYS CHOSEN, IS THAT OKAY?
-                current_root = max(action_values, key=action_values.get)  # P1 chooses action argmax(Q + u)
-            elif current_root.get_player() == 2:
-                current_root = min(action_values, key=action_values.get)  # P2 chooses action argmin(Q - u)
-            child_nodes = current_root.get_child_nodes()
-        return current_root  # The chosen leaf node
+            if current_node.get_player() == 1:
+                current_node = max(child_values, key=child_values.get)  # P1 chooses action argmax(Q + u)
+            elif current_node.get_player() == 2:
+                current_node = min(child_values, key=child_values.get)  # P2 chooses action argmin(Q - u)
+            child_nodes = current_node.get_child_nodes()
+        return current_node  # The chosen leaf node
 
-    def leaf_node_expansion(self, node):
+    def leaf_node_expansion(self, leaf_node):
         """ The child states of a parent state is generated, and the tree node housing the parent state
-        (i.e. parent node) is connected to the nodes housing the child states (i.e. child nodes)"""
-        if not self.state_manager.is_winning_state(node.get_state()):  # node is a leaf node, so expansion is not possible
-            child_nodes = self.state_manager.get_child_nodes(node.get_state(), node.get_player())
-            child_player = 1 if node.player == 2 else 2
+        (i.e. leaf node) is connected to the nodes housing the child states (i.e. child nodes of the leaf)"""
+        if not self.state_manager.is_game_over(leaf_node.get_state()):  # if node is a leaf node that can be expanded (i.e. not final state)
+            child_nodes = self.state_manager.get_child_nodes(leaf_node.get_state(), leaf_node.get_player())
+            child_player = 1 if leaf_node.player == 2 else 2
             for child in child_nodes:
-                node.add_child(child)  # connect child to parent, and parent to child (both performed by add_child())
+                leaf_node.add_child(child)  # connect child to parent, and parent to child (both executed in add_child())
                 child.set_player(child_player)
 
     def leaf_evaluation(self, leaf_node):
@@ -67,11 +67,11 @@ class MonteCarloTreeSearch:
         policy (i.e. default policy) from the leaf node to a final state (i.e. winning state)."""
         state = leaf_node.get_state()
         player = leaf_node.get_player()
-        while not self.state_manager.is_winning_state(state):
-            chosen_action = self.actor.target_policy(state, player)  # Default policy = target policy since MCTS is on-policy. This policy is created by the actor NN
+        while not self.state_manager.is_game_over(state):
+            chosen_action = q(state, player)  # Default policy = target policy since MCTS is on-policy. This policy is created by the actor NN
             state = self.state_manager.get_next_state(state, chosen_action)
             player = 1 if player == 2 else 2
-        if player == 2:  # In winner state, the previous player will be the winner
+        if player == 2:  # In winner state, the previous player will be the winner, since player is swapped at the end of the while loop
             reward = 1
         else:
             reward = -1  # Maybe use 0?
@@ -79,7 +79,7 @@ class MonteCarloTreeSearch:
 
     @staticmethod
     def backpropagation(node, final_evaluation):
-        """ The evaluation of a final state is propagated back up the tree, and the value and the counter of
+        """ The evaluation of a final state is propagated back up the tree, and the value and counter of
         the nodes that are located on the path to the root are updated. These counters are later used to produce
         the action probability distribution used as target in training of the actor NN (ANET)"""
         node.update_counter()
@@ -93,39 +93,16 @@ class MonteCarloTreeSearch:
     def get_root_distribution(self, node):
         """ Method for normalizing the action counters from the root (i.e. edges to child nodes) to produce
         a probability distribution that can be used as target for training the actor network (ANET)"""
-
-        # OLD
-        distribution = [0]*self.state_manager.size**2  # The value of illegal actions will be 0, since there is no child node for illegal actions so their value in the distribution is never changed from 0
+        counters = [0]*self.state_manager.size**2  # The value of illegal actions will be 0, since there is no child node for illegal actions so their value in the distribution is never changed from 0
         for child in node.get_child_nodes():
-            action_location = child.action[0]  # The action that produces the child state from node state
+            action_location = child.action[0]  # The action that produces the child state from the state of the given node
             row, col = action_location
-            distribution[row * self.state_manager.size + col] = child.get_counter()
-
-        numpy_counters = np.asarray(distribution, dtype=np.float32)
+            counters[row * self.state_manager.size + col] = child.get_counter()
+        # Normalize the counters:
+        numpy_counters = np.asarray(counters, dtype=np.float32)
         counter_sum = np.sum(numpy_counters)
-        normalized_counters = numpy_counters/counter_sum
-        return normalized_counters
-
-
-"""        # NEW
-        board_size = size
-        distributions = [0]*board_size**2
-        for i, child in enumerate(node.get_child_nodes()):
-            col, row = child.action[0]
-            distributions[row * board_size + col] = child.get_counter()
-        total = np.sum(distributions)
-        normalized_distribution = np.array(distributions)/total
-        D = tuple(normalized_distribution)
-        print("NEW: " + str(D))
-        return tuple(D)
-        
-        
-         action_counter.append(child.get_counter())
-    # print("Root: " + str(root_node) + ", Action counters: " + str(action_counter))
-    numpy_counters = np.asarray(action_counter, dtype=np.float32)
-    counter_sum = np.sum(numpy_counters)
-    normalized_counters = numpy_counters/counter_sum
-"""
+        normalized_distribution = numpy_counters/counter_sum
+        return normalized_distribution
 
 
 
