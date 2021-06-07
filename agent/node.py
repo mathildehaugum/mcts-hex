e0ffc09ff4aa6bf01054d57cc38792cb4346282c
mathildehaugum <mathilde_haugum@hotmail.com>
2021-06-07 10:42:54 +0200

    Fix error in node player

--- agent/node.py
+++ agent/node.py
@@ -1,15 +1,18 @@
 class Node:
     def __init__(self, state, action):
         """ Class for making nodes that represents different states in the game. A node  will contain
-        a state, the action that produced the state and the current player that will chose an action from this state. """
+        a state, the action that produced the state and the current player that will chose an action from this state"""
         self.name = "State: [" + str(state) + "], Action: " + str(action)
         self.state = state  # State when this node is root
         self.action = action  # Action taken from root leading to this child node
         self.parent = None
         self.child_nodes = []
-        self.node_value = 0  # the value of the action that leads to this node
-        self.node_counter = 0  # counts the number of times the node is visited during the search
-        self.player = action[1]  # The player that performs an action from the state of this node (i.e. decides next state)
+        self.node_value = 0  # the value of the action that leads to this node reflecting the desirability of the state
+        self.node_counter = 0  # counts the number of times the node is visited during MCTS
+        if action[0] is None:  # starting player is given in the action
+            self.player = action[1]
+        else:
+            self.player = 1 if action[1] == 2 else 2  # action[1] is the player that took the action that produced this state, while self.player is the player that performs an action from this node (i.e. decides next state)
 
     def __str__(self):
         """ Changes Node object representation to string format to make debugging easier"""
@@ -62,7 +65,7 @@ class Node:
         return self.node_counter
 
     def get_parent_counter(self):
-        """ Returns the counter of the parent node, used to calculate the value of the node during MCTS"""
+        """ Returns the counter of the parent node, used to decide the path during the tree search step of MCTS"""
         return self.parent.get_counter()
 
     def get_value(self):
